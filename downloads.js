cd0633f013715e83d1366ceba8c7581af3bb572e
braxtonmediaco <independentshadow13@gmail.com>
2026-03-29 11:58:56 -0500

    Add files via upload

---
+++ downloads.js
@@ -0,0 +1,42 @@
+const downloadLinksData = [    
+    {
+        title: "2026-03-26 Girls Soccer",
+        url: "https://photos.google.com/share/AF1QipO5eA6zNbkfk8SWbbBeZzse3PYkK1eL0ryUFOvE36HjIqBHiRRengxGAju1V8THMA?key=b2FITUJZcVVEdkFzREZZUnhKcnBWaDltdDRWTFlB",
+        image: "downloads/Girls Soccer.jpg"
+    },
+    {
+        title: "2026-03-19 Varsity Soccer vs GI",
+        url: "https://photos.google.com/share/AF1QipPnE8NEVEZ4C3E2htLhdskdH6jbzEYRgMsg3dn80ej-KWiK3zXkUAx1HKbpIFo4-g?key=X0JFOUMwY1ItaGdiSjU0U0hNR1JrYVhwTWkyeUN3",
+        image: "downloads/Varsity Soccer vs GI.jpg"
+    },
+    {
+        title: "2026-02-06 Choir Concert",
+        url: "https://photos.google.com/share/AF1QipNk--cGukggTrm-0JVT6ABEC_xOQv7XCWaWbEfQjNaLEoC1uInIYDinYGblImC-FQ?key=WGl4QmpHa2IwblVZZWxUOUlyWHpkVzhMZWMtOUp3",
+        image: "downloads/choir.jpg"
+    },
+    {
+        title: "2025-11-26 Jazz Ensemble",
+        url: "https://photos.google.com/share/AF1QipNLYRH_s-R2USKGkVipxiSO8RuiHSPi2W02foCFHKu6JuULC_Sxqx8zzzb34MHeGQ?key=djN4RE45VVAwOGhiZ1djdVBiQnB3NXlkdGpZWTVR",
+        image: "downloads/jazz_ensemble.jpg"
+    },
+    {
+        title: "2025-11-26 Jazz Lab Band",
+        url: "https://photos.google.com/share/AF1QipOIirWqHqM1gn2L-wL65IwGo5gKtou6_1y44gPMln_H_SvViL0q86EZh4vFaLob7w?key=UUJMczI1YlRaNXRBZGVjLUdwUnY3WGpnYXNQR2V3",
+        image: "downloads/jazz_lab.jpg"
+    },
+    {
+        title: "2025-09-26 Varsity Softball",
+        url: "https://photos.google.com/share/AF1QipNoC6A6GiVwqolVpZCbqmRUJiutRjVISdtiE2wNz5_TKxPHcip6DpijtqMNtx58JQ?key=d3dIN2VsOHFNRXMxaGZ2WG5fMXpXVG1rdDJucml3",
+        image: "downloads/softball.jpg"
+    },
+    {
+        title: "2025-09-30 Varsity VB",
+        url: "https://photos.google.com/share/AF1QipNKdFo7mU25ELN5cLYCN6PeaSkSFZsc8y60NVuoc2zhXIsGeyp9xAWcXU4magpv-w?key=OUpfaTdYMG4wMlhvVUo2Tl8xdlFMWDc0UVk0VzlB",
+        image: "downloads/volleyball.jpg"
+    },
+    {
+        title: "2026-09-09 VB Kearney High vs. Fremont",
+        url: "https://photos.google.com/share/AF1QipMyBPJniwd1p5JZFnApPObiLTmXRvCLTc0yujwjxFS1eATaKJmy8Bn3f_BGkxraNQ?key=SVdYR3NicXk2blJlY25IZXVmZ0NkSC1MUDcxUFd3",
+        image: "downloads/volleyball_fremont.jpg"
+    }
+];
