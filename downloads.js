1c92813c99fa1191e9963e7ee6020254ad097a78
braxton <brax.warr@gmail.com>
2026-08-23 09:05:35 -0500

    Downloads

--- downloads.js
+++ downloads.js
@@ -1,26 +1,6 @@
 const downloadLinksData = [   
     {
-        title: "2026-04-07 JV Track",
+        title: "2026-08-21 Tennis Scrimmage",
         url: "https://photos.app.goo.gl/wzjX5ywWxBCXxbp9A",
     }, 
-    {
-        title: "2026-03-26 Girls Soccer",
-        url: "https://photos.google.com/share/AF1QipO5eA6zNbkfk8SWbbBeZzse3PYkK1eL0ryUFOvE36HjIqBHiRRengxGAju1V8THMA?key=b2FITUJZcVVEdkFzREZZUnhKcnBWaDltdDRWTFlB",
-        image: "downloads/Girls Soccer.jpg"
-    },
-    {
-        title: "2026-03-19 Varsity Soccer vs GI",
-        url: "https://photos.google.com/share/AF1QipPnE8NEVEZ4C3E2htLhdskdH6jbzEYRgMsg3dn80ej-KWiK3zXkUAx1HKbpIFo4-g?key=X0JFOUMwY1ItaGdiSjU0U0hNR1JrYVhwTWkyeUN3",
-        image: "downloads/Varsity Soccer vs GI.jpg"
-    },
-    {
-        title: "2026-02-06 Choir Concert",
-        url: "https://photos.google.com/share/AF1QipNk--cGukggTrm-0JVT6ABEC_xOQv7XCWaWbEfQjNaLEoC1uInIYDinYGblImC-FQ?key=WGl4QmpHa2IwblVZZWxUOUlyWHpkVzhMZWMtOUp3",
-        image: "downloads/choir.jpg"
-    },
-    {
-        title: "2025-11-26 Jazz Ensemble",
-        url: "https://photos.google.com/share/AF1QipNLYRH_s-R2USKGkVipxiSO8RuiHSPi2W02foCFHKu6JuULC_Sxqx8zzzb34MHeGQ?key=djN4RE45VVAwOGhiZ1djdVBiQnB3NXlkdGpZWTVR",
-        image: "downloads/jazz_ensemble.jpg"
-    },
 ];
